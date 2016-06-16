db3ec41ef9cdd6e839acfbf268a6eacfbf6d9e49
patlockley <patrick.lockley@googlemail.com>
2016-06-17 00:33:48 +0100

    Obsolete

--- parts/content/content-category-featured.php
+++
@@ -1,7 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h2 style="<?PHP el-capitan_post_title_background(); ?>" class="entry-title">
-			<a href="<?PHP echo get_permalink(); ?>" rel="bookmark"><?PHP echo the_title(); ?></a>
-		</h2>
-	</header><!-- .entry-header -->
-</article><!-- #post-## -->
